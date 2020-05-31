da17165c7abd2b65d9fb0afe927d6a30e97de3b0
Bairamov Timur <BairamovTM@yandex.ru>
2020-05-30 20:43:46 -0700

    Рефакторинг в части получения случайных чисел и операций

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,8 +4,8 @@ import gameEngine from '../index.js';
 const resultRound = () => {
   const first = getRandomNumber();
   const step = getRandomNumber();
-  const hiddenPositionIndex = getRandomNumber() - 1;
   const sizeArr = 10;
+  const hiddenPositionIndex = getRandomNumber(0, sizeArr - 1);
   const progression = [...Array(sizeArr)].map((elem, ind) => first + step * ind);
   const correctAnswer = String(progression[hiddenPositionIndex]);
   progression[hiddenPositionIndex] = '..';
--- src/rand.js
+++ src/rand.js
@@ -1,16 +1,13 @@
-
-const getRandomInt = (min, max) => {
+const getRandomNumber = (min = 1, max = 11) => {
   const minRound = Math.ceil(min);
   const maxRound = Math.floor(max);
   return Math.floor(Math.random() * (maxRound - minRound)) + minRound;
 };
 
-const getRandomNumber = () => getRandomInt(1, 11);
-
 const getRandomMatematicOperation = () => {
   const operations = ['+', '-', '*'];
-  const number = getRandomInt(1, operations.length);
-  return operations[number - 1];
+  const index = getRandomNumber(0, operations.length);
+  return operations[index];
 };
 
 export default getRandomNumber;
